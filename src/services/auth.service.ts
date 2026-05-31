567ca77d30cafd07d282938c91b579074701782c
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2026-05-31 16:23:25 +0530

    refactor: update auth.service.ts for AuthResponse return data

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,24 +1,23 @@
 import { axiosInstance } from "../lib/axios";
-import type { LoginPayload, RegisterPayload } from "../types/auth.types";
+
+import type {
+  AuthResponse,
+  LoginPayload,
+  RegisterPayload,
+} from "../types/auth.types";
 
 export const registerUser = async (
   payload: RegisterPayload,
-) => {
-  const response = await axiosInstance.post(
-    "/auth/register",
-    payload,
-  );
+): Promise<AuthResponse> => {
+  const response = await axiosInstance.post("/auth/register", payload);
 
   return response.data;
 };
 
 export const loginUser = async (
   payload: LoginPayload,
-) => {
-  const response = await axiosInstance.post(
-    "/auth/login",
-    payload,
-  );
+): Promise<AuthResponse> => {
+  const response = await axiosInstance.post("/auth/login", payload);
 
   return response.data;
 };
