59e8ebf460e080b1cc2f865f2fbc9f3dc0b7ba5a
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2026-05-31 16:16:10 +0530

    Setup: Modular Architecture and Routes in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { App } from './App'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HomePage } from './pages/HomePage'
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element #root not found");
+}
+
+createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route index element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+export const HomePage = () => {
+    return (
+        <div className="flex h-screen items-center justify-center">
+            <h1 className="text-4xl font-bold">
+                ChatGPT UI
+            </h1>
+        </div>
+    )
+}
